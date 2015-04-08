932fd9fb632efe91897952ad703564c6c726711a
OzuYatamutsu <scollins36@gatech.edu>
2015-04-08 12:25:20 -0400

    Add multiple test bytestream cases

--- client-test.py
+++ client-test.py
@@ -2,7 +2,16 @@ from rat import RatSocket
 from sys import argv
 
 TEST_BYTESTREAM = b"Make sure to drink your ovaltine."
+TEST_BYTESTREAM2 = b"All commit and no push makes Jinhai a big risk"
+TEST_BYTESTREAM3 = b"If you have 8 AM classes, I feel bad for ya son"
+TEST_BYTESTREAM4 = b"I've got 99 problems, but they start at 1"
+TEST_LONGSTREAM = b"""This is a long bytestream that must be split into multiple segments.
+This is line two of a long bytestream that must be split into multiple segments.
+This is line three of a long bytestream that must be split into multiple segments.
+This is line four of a long bytestream that must be split into multiple segments."""
 
+send_queue = [TEST_BYTESTREAM, TEST_BYTESTREAM2, TEST_BYTESTREAM3, 
+              TEST_BYTESTREAM4, TEST_LONGSTREAM]
 def main():
     client_sock = RatSocket(True)
     print("New RatSocket construction successful!")
@@ -10,7 +19,8 @@ def main():
     client_sock.connect("127.0.0.1", int(argv[1]))
     print("RatSocket connected to 127.0.0.1:" + argv[1] + "!")
 
-    client_sock.send(TEST_BYTESTREAM)
-    print("RatSocket sent datagram!")
+    for item in send_queue:
+        client_sock.send(TEST_BYTESTREAM)
+        print("RatSocket sent stream " + str(send_queue.index(item) + 1) + "to server!")
 
 main()
--- server-test.py
+++ server-test.py
@@ -2,6 +2,16 @@ from rat import RatSocket, RAT_HEADER_SIZE
 from sys import argv
 
 TEST_BYTESTREAM = b"Make sure to drink your ovaltine."
+TEST_BYTESTREAM2 = b"All commit and no push makes Jinhai a big risk"
+TEST_BYTESTREAM3 = b"If you have 8 AM classes, I feel bad for ya son"
+TEST_BYTESTREAM4 = b"I've got 99 problems, but they start at 1"
+TEST_LONGSTREAM = b"""This is a long bytestream that must be split into multiple segments.
+This is line two of a long bytestream that must be split into multiple segments.
+This is line three of a long bytestream that must be split into multiple segments.
+This is line four of a long bytestream that must be split into multiple segments."""
+
+recv_queue = [TEST_BYTESTREAM, TEST_BYTESTREAM2, TEST_BYTESTREAM3, 
+              TEST_BYTESTREAM4, TEST_LONGSTREAM]
 
 def main():
     server_sock = RatSocket(True)
@@ -14,7 +24,8 @@ def main():
     if client is not False: print("RatSocket accepted connection from client!")
     else: print("Error: RatSocket didn't receive ACK response!")
 
-    test_data = server_sock.recv(len(TEST_BYTESTREAM) + RAT_HEADER_SIZE)
-    if test_data == TEST_BYTESTREAM: print("RatSocket successfully receieved datagram from client!")
+    for item in recv_queue:
+        test_data = server_sock.recv(len(item) + RAT_HEADER_SIZE)
+        if test_data == item: print("RatSocket successfully receieved datagram from client!")
 
 main()
